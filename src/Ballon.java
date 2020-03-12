9717d38d9e9e49e1d447fcfb9e739da12f36dca2
931701723 <931701723@E2-P406-16.galaxy.cosmos.local>
2020-03-12 21:20:07 +0100

    Comm Ballon correct objet

--- src/Ballon.java
+++ src/Ballon.java
@@ -6,12 +6,14 @@ import java.math.*;
 public class Ballon extends Objet{
 
 	
+	public Ballon()
+	{
+	
+	}
 
-
-
- 	 public boolean hitboxfast (Objet tocheck)
-{
-	 boolean result=false;
+	public boolean hitboxfast (Objet tocheck)
+	{
+		boolean result=false;
 	 double  i;
 	 double bx,by;
 	 for (i=0;i<3.14;i+=3.14/8)// A opti en finissant si colli ? 
@@ -39,19 +41,25 @@ public class Ballon extends Objet{
  	 public boolean hitboxslow (Objet tocheck)
  	 {
  		 boolean test=false;
- 		 if (x+r>=tocheck.r)
+ 		 if (x+r>=tocheck.r)// Lire les point cardinaux du ballon W
  		 {
- 			 if(x-r<=tocheck.x+tocheck.r)
+ 			 if(x-r<=tocheck.x+tocheck.r)//E
  			 {
- 				if (y+r>=tocheck.r)
+ 				if (y+r>=tocheck.r)//N
  				{
- 		 			 if(y-r<=tocheck.y+tocheck.r)
+ 		 			 if(y-r<=tocheck.y+tocheck.r)//S
  		 			 {
- 		 				if (Math.sqrt((tocheck.x*tocheck.x-x*x + tocheck.y*tocheck.y-y*y))<r)
+ 		 				if (Math.sqrt((tocheck.x*tocheck.x-x*x + tocheck.y*tocheck.y-y*y))<r)// Distance par rapport aux coins HG
  		 				{
- 		 					if (Math.sqrt((tocheck.x+tocheck.r)*tocheck.x-x*x + tocheck.y*tocheck.y-y*y)<r)
+ 		 					if (Math.sqrt((tocheck.x+tocheck.r)*(tocheck.x+tocheck.r) + tocheck.y*tocheck.y-y*y)<r)//HD
  	 		 				{
- 		 						
+ 		 		 				if (Math.sqrt((tocheck.x*tocheck.x-x*x + (tocheck.y+r)*(tocheck.y+r)-y*y))<r)//BG
+ 		 		 				{
+ 		 		 					if (Math.sqrt((tocheck.x+tocheck.r)*(tocheck.x+tocheck.r) + (tocheck.y+r)*(tocheck.y+r)-y*y)<r)//BD
+ 		 	 		 				{
+ 		 		 						
+ 		 							}
+ 								}
  							}
 						}
  		 			 }
--- src/Objet.java
+++ src/Objet.java
@@ -6,12 +6,14 @@ import javax.swing.*;
 
 
 public class Objet extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
 	protected  static double r;
 	protected double x=0,y=0;
 	protected  JPanel img;
 	protected  int hitbox;
 	protected  int type; 
-	 public Objet(double _x,double _y)
+	public Objet(double _x,double _y)
 	 {
 		 x=_x;
 		 y=_y;
